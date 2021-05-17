c8cf2d5f1383024f0bf213489ff664af21743154
KostasThan <36800742+KostasThan@users.noreply.github.com>
2021-05-17 22:25:27 +0300

    init

--- src/tests/spheregame/AStarAlgorithmTest.java
+++ src/tests/spheregame/AStarAlgorithmTest.java
@@ -1,42 +1,40 @@
-package tests.spheregame;
-
-import static org.junit.Assert.assertTrue;
-
-import org.junit.jupiter.api.RepeatedTest;
-import org.junit.jupiter.api.Test;
-
-import games.spheregame.model.SphereGame;
-import games.spheregame.searchalgorithms.AStarAlgorithm;
-import games.spheregame.searchalgorithms.SphereGameSearch;
-import games.spheregame.searchalgorithms.UCSAlgorithm;
-
-class AStarAlgorithmTest {
-
-	
-	
-	
-	
-	@RepeatedTest(value = 100)
-	void test() {
-		
-		SphereGame game = new SphereGame(3);
-		
-
-			SphereGameSearch bot = new SphereGameSearch(game,new UCSAlgorithm(),false);
-			AStarAlgorithm astar = new AStarAlgorithm();
-			int actuallCost;
-			int guess;
-			for (int tries = 0; tries < 1000; tries++) {
-
-				actuallCost = bot.findSolution(false);
-				guess = astar.guessEndGame(game);
-
-
-				assertTrue(guess <= actuallCost);
-
-				game.reset();
-			}
-		
-	}
-
-}
+//package tests.spheregame;
+//
+//import games.spheregame.model.SphereGame;
+//import games.spheregame.searchalgorithms.AStarAlgorithm;
+//import games.spheregame.searchalgorithms.SphereGameSearch;
+//import games.spheregame.searchalgorithms.UCSAlgorithm;
+//import org.junit.jupiter.api.RepeatedTest;
+//
+//import static org.junit.Assert.assertTrue;
+//
+//class AStarAlgorithmTest {
+//
+//
+//
+//
+//
+//	@RepeatedTest(value = 100)
+//	void test() {
+//
+//		SphereGame game = new SphereGame(3);
+//
+//
+//			SphereGameSearch bot = new SphereGameSearch(game,new UCSAlgorithm(),false);
+//			AStarAlgorithm astar = new AStarAlgorithm();
+//			int actuallCost;
+//			int guess;
+//			for (int tries = 0; tries < 1000; tries++) {
+//
+//				actuallCost = bot.findSolution(false);
+//				guess = astar.guessEndGame(game);
+//
+//
+//				assertTrue(guess <= actuallCost);
+//
+//				game.reset();
+//			}
+//
+//	}
+//
+//}
